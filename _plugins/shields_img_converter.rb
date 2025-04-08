396983a41bc3975d348a0cf8d07d23edda9aae86
winny mac <wanyutang@gmail.com>
2025-04-08 14:41:48 +0800

    refactor: update shields image converter to use site post_write hook for HTML processing

--- _plugins/shields_img_converter.rb
+++ _plugins/shields_img_converter.rb
@@ -1,10 +1,14 @@
 # _plugins/shields_img_converter.rb
 
 module Jekyll
-  Jekyll::Hooks.register :documents, :post_render do |document|
-    document.content = document.content.gsub(/<a href="(https:\/\/img\.shields\.io\/.+?)".*?><img src="(https:\/\/img\.shields\.io\/.+?)".*?><\/a>/) do
-      url = Regexp.last_match(2)
-      "<img src='#{url}' alt='shields badge' loading='lazy'>"
+  Jekyll::Hooks.register :site, :post_write do |site|
+    Dir.glob("_site/**/*.html").each do |file|
+      content = File.read(file)
+      content.gsub!(/<a[^>]*href="(https:\/\/img\.shields\.io\/[^"]+)"[^>]*>\s*<img[^>]*src="(https:\/\/img\.shields\.io\/[^"]+)"[^>]*>\s*<\/a>/) do
+        url = Regexp.last_match(2)
+        "<img src='#{url}' alt='shields badge' loading='lazy'>"
+      end
+      File.write(file, content)
     end
   end
 end
