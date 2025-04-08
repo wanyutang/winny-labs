18118584f64fee0ade0971965018e6d08af6078e
winny mac <wanyutang@gmail.com>
2025-04-08 14:47:35 +0800

    refactor: update shields image converter to remove popup class from anchor tags

--- _plugins/shields_img_converter.rb
+++ _plugins/shields_img_converter.rb
@@ -4,9 +4,12 @@ module Jekyll
   Jekyll::Hooks.register :site, :post_write do |site|
     Dir.glob("_site/**/*.html").each do |file|
       content = File.read(file)
-      content.gsub!(/<a[^>]*href="(https:\/\/img\.shields\.io\/[^"]+)"[^>]*>\s*<img[^>]*src="(https:\/\/img\.shields\.io\/[^"]+)"[^>]*>\s*<\/a>/) do
-        url = Regexp.last_match(2)
-        "<img src='#{url}' alt='shields badge' loading='lazy'>"
+      # 只移除含 shields 圖片的 <a> 的 popup class
+      content.gsub!(/<a([^>]*)class="([^"]*)popup([^"]*)"([^>]*)>\s*<img[^>]*src="https:\/\/img\.shields\.io\/[^"]+"[^>]*>/) do
+        other_classes = [Regexp.last_match(2), Regexp.last_match(3)].join.strip
+        other_classes = other_classes.gsub(/\s+/, ' ') # 去掉多餘空白
+        other_classes = other_classes.empty? ? '' : " class=\"#{other_classes}\""
+        "<a#{Regexp.last_match(1)}#{other_classes}#{Regexp.last_match(4)}>"
       end
       File.write(file, content)
     end
