90f1380b4d3151e8f66aff8e237f630f4b2b665d
winny mac <wanyutang@gmail.com>
2025-04-08 13:03:38 +0800

    refactor: rename CustomImgConverter to UnifiedMarkdownConverter and enhance image handling logic

--- _plugins/custom_img_converter.rb
+++ _plugins/custom_img_converter.rb
@@ -1,10 +1,22 @@
+# _plugins/custom_img_converter.rb
 module Jekyll
-  class CustomImgConverter < Jekyll::Converters::Markdown
+  class UnifiedMarkdownConverter < Jekyll::Converters::Markdown
     def convert(content)
-      # 使用正則表達式匹配 shields.io 的圖片網址
-      content = content.gsub(/<a href="(https:\/\/img\.shields\.io\/.+?)".*?><img src="(https:\/\/img\.shields\.io\/.+?)".*?><\/a>/) do
-        # 只保留圖片的 <img> 標籤
-        "<img src='#{Regexp.last_match(2)}' alt='shields badge' loading='lazy'>"
+      # 處理 `![[filename | width]]` 或 `![[filename]]` 特定格式
+      content = content.gsub(/!\[\[(.+?)(?:\s*\|\s*(\d+))?\]\]/) do
+        filename = Regexp.last_match(1).strip
+        width = Regexp.last_match(2)
+        if width
+          "<img src='/assets/images/#{filename}' width='#{width}' alt='#{filename}' />"
+        else
+          "<img src='/assets/images/#{filename}' alt='#{filename}' />"
+        end
+      end
+
+      # 處理 shields.io 的 Markdown 圖片
+      content = content.gsub(/!\[.*?\]\((https:\/\/img\.shields\.io\/.+?)\)/) do
+        url = Regexp.last_match(1)
+        "<img src='#{url}' alt='shields badge' loading='lazy'>"
       end
 
       # 調用原始 Markdown 渲染邏輯處理其他內容
